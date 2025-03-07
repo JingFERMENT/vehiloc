8a76ff983670f2040d328162cbc91828604d73d1
Jing ZHANG <jing.zhang.ferment@gmail.com>
2025-03-07 07:32:37 +0100

    delete a car

--- src/Controller/VoituresController.php
+++ src/Controller/VoituresController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\VoitureRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class VoituresController extends AbstractController
 {
+    // display all vehicles
     #[Route('/accueil', name: 'app_accueil')]
     public function index(VoitureRepository $respository): Response
     {
@@ -19,12 +22,13 @@ final class VoituresController extends AbstractController
         ]);
     }
 
-
-    #[Route('/voiture/{id}', name: 'app_voiture', requirements: ['id' => '\d+'])]
-    public function voiture(int $id, VoitureRepository $respository): Response
+    // display one vehicle
+    #[Route('/voiture/{id}', name: 'app_show', requirements: ['id' => '\d+'])]
+    public function show(VoitureRepository $respository,int $id, ): Response
     {
         $voiture = $respository->find($id);
         
+        // if no vehicle correspond, redirect to the home page
         if(!$voiture){
             return $this->redirectToRoute('app_accueil');
         }
@@ -33,5 +37,23 @@ final class VoituresController extends AbstractController
             'voiture' => $voiture,
         ]);
     }
+
+    // delete one vehicle 
+    #[Route('/voiture/{id}/supprimer', name: 'app_voiture_delete')]
+    public function delete(int $id, VoitureRepository $respository, EntityManagerInterface $entityManager): Response
+    {
+       
+        $voiture = $respository->find($id);
+        
+        if(!$voiture){
+            return $this->redirectToRoute('app_accueil');
+        }
+
+        $entityManager->remove($voiture); // remove method to delete the vehicle
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_accueil');
+    }
+
     
 }
